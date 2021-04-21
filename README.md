19a7d4ce1375fa993dc81a97be3b6b3f015ff450
CargoDankster <videogamefpslover@gmail.com>
2021-04-21 14:12:14 -0400

    almost got things moving, literally

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# chester
-a chess bot for discord
+# Chester
+A bot that allows two player to play chess against each other.
+
+## Commands
+- .showBoard
+  - Shows the board currently active in the channel
+- .challenge (player)
+  - Challenges (player) to a game of chess. Also used to accept a challenge.
+- .move (position) (destination)
+  - Move a piece at (position) to (destination)
+- .forfeit
+  - If you suck, you can forfeit a match
--- main.js
+++ main.js
@@ -10,25 +10,11 @@ const dotenv = require('dotenv');
 const client = new Discord.Client();
 dotenv.config();
 
-/*
-handles moving a chess piece
-*/
-class MoveHandler
-{
-    constructor()
-    {
-    }
-    move()
-    {}
-    capture()
-    {}
-}
-
 class Game
 {
     genBoardString()
     {
-        this.boardString = "```"
+        this.boardString = "```\n"
         for (var y = 0; y < this.board.length; y++)
         {
             for (var x = 0; x < this.board.length; x++)
@@ -38,7 +24,7 @@ class Game
             this.boardString += "\n"
         }
         this.boardString += "```"
-
+        return this.boardString
     }
     constructor()
     {
@@ -52,15 +38,92 @@ class Game
             ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
             ['♖', '♘', '♗', '♔', '♕', '♗', '♘', '♖']
         ];
+        this.blankBoard = [
+            ['◻️', '◼️', '◻️', '◼️', '◻️', '◼️', '◻️', '◼️'],
+            ['◼️', '◻️', '◼️', '◻️', '◼️', '◻️', '◼️', '◻️'],
+            ['◻️', '◼️', '◻️', '◼️', '◻️', '◼️', '◻️', '◼️'],
+            ['◼️', '◻️', '◼️', '◻️', '◼️', '◻️', '◼️', '◻️'],
+            ['◻️', '◼️', '◻️', '◼️', '◻️', '◼️', '◻️', '◼️'],
+            ['◼️', '◻️', '◼️', '◻️', '◼️', '◻️', '◼️', '◻️'],
+            ['◻️', '◼️', '◻️', '◼️', '◻️', '◼️', '◻️', '◼️'],
+            ['◼️', '◻️', '◼️', '◻️', '◼️', '◻️', '◼️', '◻️']
+        ]
         this.genBoardString()
+    }
+    ReplacePiece(y1, x1, y2, x2)
+    {
+        this.board[y2][x2] = this.board[y1][x1]
+        this.board[y1][x1] = this.blankBoard[y1][x1]
+    }
+    Occupied(y, x)
+    {
+        if (this.board[y][x] == '◼️' || this.board[y][x] == '◻️')
+            return false;
+        return true;
+    }
+    MakeMove(condition, y1, x1, y2, x2)
+    {
+        if (condition) {
+            this.ReplacePiece(y1, x1, y2, x2)
+            return true;
+        } return false;
+    }
+    Move(y1, x1, y2, x2)
+    {
+        var xDif = x2-x1;
+        var yDif = y2-y1;
+        if (y1 == y2 && x1 == x2) return false;
+        //if (this.Occupied(y2, x2)) return false;
+        switch (this.board[y1][x1])
+        {
+            case '♜':
+            case '♖':
+                return this.MakeMove(
+                    ((x1 == x2) || (y1 == y2)),
+                    y1, x1, y2, x2)
+            case '♗':
+            case '♝':
+                return this.MakeMove(
+                    (
+                        (x2-x1)/(y2-y1) == 1 ||
+                        (x2-x1)/(y2-y1) == -1
+                    ),
+                    y1, x1, y2, x2)
+            case '♕':
+            case '♛':
+                return this.MakeMove(
+                    (
+                        ((xDif)/(yDif) == 1 || 
+                        (xDif)/(yDif) == -1) || 
+                        (x1 == x2) || (y1 == y2)),
+                    y1, x1, y2, x2)
+            case '♚':
+            case '♔':
+                return this.MakeMove(
+                    (2 > xDif > -2 && 2 > yDif > -2),
+                    y1, x1, y2, x2)
+            case '♘':
+            case '♞':
+                return this.MakeMove(
+                    (
+                        (xDif == 2 && yDif == 1) ||
+                        (xDif == 1 && yDif == 2) ||
+                        (xDif == -2 && yDif == -1) ||
+                        (xDif == -1 && yDif == -2)
+                    ),
+                    y1, x1, y2, x2)
+            default:
+                return false;
+        }
     }
 }
 
 const board = new Game();
-const moveManager = new MoveHandler();
 
 client.on("ready", () => {
     console.log("E7 -> E5");
+    console.log(board.Move(0, 2, 1, 1))
+    console.log(board.genBoardString())
 });
 
 client.on("message", msg => {
